b8a40c30eef59ce4ef20f1af417035d8de52d189
shiv <sdeshmukh@tavisca.com>
2017-04-26 15:27:23 +0530

    set cookie

---
+++ AuthStealer.js
@@ -0,0 +1,6 @@
+// This code sets the cookie, I need this for my testing. you do not need this.
+$.cookie("username", "dhananjay");
+$.cookie("password", "CCSU"); 
+
+console.log($.cookie("username"));
+console.log($.cookie("password"));
