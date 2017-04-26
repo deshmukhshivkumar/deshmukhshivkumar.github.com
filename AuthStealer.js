20ddf6de8218c603f3fb8dae2437abcde16968d0
shiv <sdeshmukh@tavisca.com>
2017-04-26 17:13:01 +0530

    commit 2

--- AuthStealer.js
+++ AuthStealer.js
@@ -1,26 +1,10 @@
- function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
- 
  $( function() {
 	// This code sets the cookie, I need this for my testing. you do not need this.
-	document.cookie = "username=dhananjay";
-	document.cookie = "password=CCSU"; 
+	$.cookie('username', 'dhananjay');
+	$.cookie('password', 'CCSU'); 
 
-	console.log(getCookie("username"));
-	console.log(getCookie("password"));
+	console.log($.cookie('username'));
+	console.log($.cookie('password'));
 	
 });
 
